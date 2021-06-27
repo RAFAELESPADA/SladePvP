5b7aa3cb776f134595d4220e73425c2e19b840d1
Smart <gustavotropper@gmail.com>
2021-06-27 17:32:09 -0300

    Corrigido bug do jump

--- src/net/helix/pvp/listener/Jump.java
+++ src/net/helix/pvp/listener/Jump.java
@@ -22,7 +22,7 @@ public class Jump implements Listener {
 		Player p = e.getPlayer();
 		if (e.getTo().getBlock().getRelative(BlockFace.DOWN).getType() == org.bukkit.Material.SPONGE) {
 			jump.remove(p.getName());
-			Vector v = p.getLocation().getDirection().multiply(0).setY(5.5F);
+			Vector v = p.getLocation().getDirection().setY(5.5F);
 			p.setVelocity(v);
 			jump.add(p.getName());
 		}
