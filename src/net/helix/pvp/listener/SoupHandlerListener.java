47d9cac76a38ee4b2ae663eac79a95b78048fe90
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2023-08-01 21:03:29 -0300

    Update SoupHandlerListener.java

--- src/net/helix/pvp/listener/SoupHandlerListener.java
+++ src/net/helix/pvp/listener/SoupHandlerListener.java
@@ -18,26 +18,18 @@ import net.helix.pvp.kit.provider.HelixActionBar;
 
 public class SoupHandlerListener implements Listener {
 	
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onTomarSopa(PlayerInteractEvent e) {
 		Player p = e.getPlayer();
 		
 		if (e.getAction().equals(Action.RIGHT_CLICK_AIR) || (e.getAction().equals(Action.RIGHT_CLICK_BLOCK))) {
 			if (p.getItemInHand().getType() == Material.MUSHROOM_SOUP) {
 				if ((p.getHealth() < 20.0D)) {
-					if (e.isCancelled()) {
-						e.setCancelled(true);
-						p.setHealth(p.getHealth() + 7 >= 20.0D ? 20.0D : p.getHealth() + 7);
-						p.setFoodLevel(20);
-						p.setItemInHand(KitManager.getPlayer(p.getName()).hasKit(HelixKit.QUICKDROPPER) || KitManager2.getPlayer(p.getName()).haskit2(HelixKit2.QUICKDROPPER) ? new ItemStack(Material.AIR) : new ItemStack(Material.BOWL));
-						HelixActionBar.send(p, "§c+" + (p.getHealth() + 7 <= 20.0D ? 3.5 : 3.5) + " §4§l\u2661");
-						p.updateInventory();	
-					}
 					e.setCancelled(true);
 					p.setHealth(p.getHealth() + 7 >= 20.0D ? 20.0D : p.getHealth() + 7);
 					p.setFoodLevel(20);
 					p.setItemInHand(KitManager.getPlayer(p.getName()).hasKit(HelixKit.QUICKDROPPER) || KitManager2.getPlayer(p.getName()).haskit2(HelixKit2.QUICKDROPPER) ? new ItemStack(Material.AIR) : new ItemStack(Material.BOWL));
-					HelixActionBar.send(p, "§c+" + (p.getHealth() + 7 <= 20.0D ? 3.5 : 3.5) + " §4§l\u2661");
+					HelixActionBar.send(p, "§c+" + (p.getHealth() + 7 <= 20.0D ? 3.5 : 3.5) + " §4§l❤️");
 					p.updateInventory();
 				}
 			}
