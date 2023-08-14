017b9e19471d9d3983d6c8df87d0aeec02849b8d
RAFAELESPADA <pidgeypidgey1@gmail.com>
2023-08-13 23:08:14 -0300

    Fixed /finalizarbatalhas

--- src/net/helix/pvp/command/RemoverGlads.java
+++ src/net/helix/pvp/command/RemoverGlads.java
@@ -9,7 +9,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import net.helixpvp.kit2.GladiatorListener;
+import net.helix.pvp.kit.provider.GladiatorListener;
 
 public class RemoverGlads 
 
