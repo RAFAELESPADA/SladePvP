fdc01a34c69fc08608a28d3a5c10854047e5e435
RAFAELESPADA <pidgeypidgey1@gmail.com>
2023-08-15 18:38:07 -0300

    1V1

--- src/net/helix/pvp/command/OneVsOneIniciar.java
+++ src/net/helix/pvp/command/OneVsOneIniciar.java
@@ -90,7 +90,6 @@ public boolean onCommand(CommandSender sender, Command cmd, String label, String
 			 	 EventoUtils.getEventoPlayers().forEach(p2 -> {
 			 		 if (!RDMAutomatic.iniciou) {
 			 		RDMAutomatic.iniciou = true;
-			 		RDMAutomatic.playersIN.add(p2.getName());
 			 		Bukkit.getConsoleSender().sendMessage("Variavel de inicio do evento 1v1 setada como true");
 			 		 }
 			 		
